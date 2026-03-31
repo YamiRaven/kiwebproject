028132c74480903792559088a16a8ca26188a1ad
YamiRaven <tanja.schnyder@stud.hslu.ch>
2026-03-31 13:30:47 +0200

    - input Hanna

--- script.js
+++ script.js
@@ -6,22 +6,25 @@ const scroller = scrollama();
 scroller.setup({
     step: ".content-section",
     offset: 0.8,
-    debug: true
+    debug: false
 });
 
 scroller
     .onStepEnter((response) => {
         const heading = response.element.querySelector(".subheading");
         if (heading) {
-            heading.classList.add("is-active");
+            heading.classList.remove("is-inactive");
         }
     })
+    
     .onStepExit((response) => {
         const heading = response.element.querySelector(".subheading");
         if (heading) {
-            heading.classList.remove("is-active");
+            heading.classList.add("is-inactive");
         }
-    });
+    })
+    ;
+    
 
 window.addEventListener("resize", () => {
     scroller.resize();
